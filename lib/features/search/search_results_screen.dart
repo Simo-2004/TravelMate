eee492686d652ef6389e824fbb593ab26742e576
Simo-2004 <153453435+Simo-2004@users.noreply.github.com>
2026-07-11 11:29:07 +0200

    fix: Duplicated line of code

--- lib/features/search/search_results_screen.dart
+++ lib/features/search/search_results_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:travelmate/core/constants/app_sizes.dart';
 import 'package:travelmate/core/constants/app_strings.dart';
-import 'package:travelmate/features/search/mate_details_screen.dart';
-import 'package:travelmate/features/schedule/travel_schedule_screen.dart';
+import 'package:travelmate/features/search/search_mode_view.dart';
 import 'package:travelmate/shared/data/mate_catalog.dart';
 import 'package:travelmate/shared/data/trip_catalog.dart';
 import 'package:travelmate/shared/models/mate_profile.dart';
@@ -12,10 +10,6 @@ import 'package:travelmate/shared/models/trip_tile_data.dart';
 import 'package:travelmate/shared/state/search_research_mode_store.dart';
 import 'package:travelmate/shared/utils/mate_search.dart';
 import 'package:travelmate/shared/utils/trip_search.dart';
-import 'package:travelmate/shared/widgets/mates_vertical_section.dart';
-import 'package:travelmate/shared/widgets/search_bar.dart';
-import 'package:travelmate/shared/widgets/search_mode_switch_button.dart';
-import 'package:travelmate/shared/widgets/trips_vertical_section.dart';
 
 class SearchResultsScreen extends StatefulWidget {
   final String initialQuery;
@@ -45,109 +39,32 @@ class _SearchResultsScreenState extends State<SearchResultsScreen> {
     super.dispose();
   }
 
-  List<TripTileData> _filterTrips(String query) {
-    return filterTrips(_tripTiles, query, limit: _maxTripsShown);
-  }
-
-  List<MateProfile> _filterMates(String query) {
-    return filterMates(_mates, query, limit: _maxMatesShown);
-  }
-
-  void _openMateDetails(MateProfile mate) {
-    Navigator.of(
-      context,
-    ).push(MaterialPageRoute(builder: (_) => MateDetailsScreen(mate: mate)));
-  }
-
-  void _openTripSchedule(TripTileData trip) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) => TravelScheduleScreen(
-          tripId: trip.tripId,
-          tripName: trip.destinationTitle,
-          images: trip.scheduleImages,
-          tags: trip.tags,
-          destinationTitle: trip.destinationTitle,
-          destinationDescription: trip.description,
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final sizes = AppSizes.of(context);
-    final modeButtonSize = sizes.padL * 2.6;
-    final bottomClearance = modeButtonSize + sizes.padS * 2;
-
     return ValueListenableBuilder<SearchResearchMode>(
       valueListenable: SearchResearchModeStore.instance,
       builder: (context, mode, _) {
-        final filteredTrips = _filterTrips(_searchController.text);
-        final filteredMates = _filterMates(_searchController.text);
-        final hintText = mode == SearchResearchMode.trips
-            ? AppStrings.searchTripHint
-            : AppStrings.searchMateHint;
-
         return Scaffold(
           appBar: AppBar(title: const Text(AppStrings.pageSearchTitle)),
-          body: SafeArea(
-            child: Stack(
-              children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(
-                    sizes.padL,
-                    sizes.padL,
-                    sizes.padL,
-                    bottomClearance,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      TravelSearchBar(
-                        controller: _searchController,
-                        hintText: hintText,
-                        onChanged: (_) => setState(() {}),
-                        onSubmitted: (_) => setState(() {}),
-                        textInputAction: TextInputAction.done,
-                        autofocus: false,
-                      ),
-                      if (mode == SearchResearchMode.mates)
-                        Expanded(
-                          child: MatesVerticalSection(
-                            title: AppStrings.searchMatesTitle,
-                            mates: filteredMates,
-                            emptyMessage: AppStrings.searchNoMatesMessage,
-                            listHeight: double.infinity,
-                            onMateTap: _openMateDetails,
-                          ),
-                        )
-                      else
-                        Expanded(
-                          child: TripsVerticalSection(
-                            title: AppStrings.searchTripsTitle,
-                            trips: filteredTrips,
-                            emptyMessage: AppStrings.searchNoTripsMessage,
-                            listHeight: double.infinity,
-                            onTripTap: _openTripSchedule,
-                          ),
-                        ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  right: sizes.padL,
-                  bottom: sizes.padS,
-                  child: SearchModeSwitchButton(
-                    mode: mode,
-                    onTap: SearchResearchModeStore.instance.toggle,
-                    tripsLabel: AppStrings.searchModeTripsLabel,
-                    matesLabel: AppStrings.searchModeMatesLabel,
-                    size: modeButtonSize,
-                  ),
-                ),
-              ],
-            ),
+          body: SearchModeView(
+            mode: mode,
+            controller: _searchController,
+            filteredMates: mode == SearchResearchMode.mates
+                ? filterMates(
+                    _mates,
+                    _searchController.text,
+                    limit: _maxMatesShown,
+                  )
+                : const <MateProfile>[],
+            filteredTrips: mode == SearchResearchMode.trips
+                ? filterTrips(
+                    _tripTiles,
+                    _searchController.text,
+                    limit: _maxTripsShown,
+                  )
+                : const <TripTileData>[],
+            onChanged: (_) => setState(() {}),
+            onSubmitted: (_) => setState(() {}),
           ),
         );
       },
--- lib/features/search/search_screen.dart
+++ lib/features/search/search_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:travelmate/core/constants/app_sizes.dart';
 import 'package:travelmate/core/constants/app_strings.dart';
 import 'package:travelmate/features/navigation/navigation_controller.dart';
-import 'package:travelmate/features/schedule/travel_schedule_screen.dart';
-import 'package:travelmate/features/search/mate_details_screen.dart';
+import 'package:travelmate/features/search/search_mode_view.dart';
 import 'package:travelmate/features/search/search_results_screen.dart';
 import 'package:travelmate/shared/data/mate_catalog.dart';
 import 'package:travelmate/shared/data/trip_catalog.dart';
@@ -14,10 +12,6 @@ import 'package:travelmate/shared/models/trip_tile_data.dart';
 import 'package:travelmate/shared/state/search_research_mode_store.dart';
 import 'package:travelmate/shared/utils/mate_search.dart';
 import 'package:travelmate/shared/utils/trip_search.dart';
-import 'package:travelmate/shared/widgets/mates_vertical_section.dart';
-import 'package:travelmate/shared/widgets/search_bar.dart';
-import 'package:travelmate/shared/widgets/search_mode_switch_button.dart';
-import 'package:travelmate/shared/widgets/trips_vertical_section.dart';
 
 /// Search tab that routes user queries to trip or mate results.
 class SearchScreen extends StatefulWidget {
@@ -76,27 +70,6 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  void _openMateDetails(MateProfile mate) {
-    Navigator.of(
-      context,
-    ).push(MaterialPageRoute(builder: (_) => MateDetailsScreen(mate: mate)));
-  }
-
-  void _openTripSchedule(TripTileData trip) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) => TravelScheduleScreen(
-          tripId: trip.tripId,
-          tripName: trip.destinationTitle,
-          images: trip.scheduleImages,
-          tags: trip.tags,
-          destinationTitle: trip.destinationTitle,
-          destinationDescription: trip.description,
-        ),
-      ),
-    );
-  }
-
   void _handleController() {
     if (!mounted || _controller == null) {
       return;
@@ -118,89 +91,29 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final sizes = AppSizes.of(context);
-    final modeButtonSize = sizes.padL * 2.6;
-    final bottomClearance = modeButtonSize + sizes.padS * 2;
-
     return ValueListenableBuilder<SearchResearchMode>(
       valueListenable: SearchResearchModeStore.instance,
       builder: (context, mode, _) {
-        final hintText = mode == SearchResearchMode.trips
-            ? AppStrings.searchTripHint
-            : AppStrings.searchMateHint;
-        final filteredMates = mode == SearchResearchMode.mates
-            ? filterMates(
-                _mates,
-                _searchController.text,
-                limit: _maxMatesShown,
-              )
-            : const <MateProfile>[];
-        final filteredTrips = mode == SearchResearchMode.trips
-            ? filterTrips(
-                _tripTiles,
-                _searchController.text,
-                limit: _maxTripsShown,
-              )
-            : const <TripTileData>[];
-
-        return SafeArea(
-          child: Stack(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(
-                  sizes.padL,
-                  sizes.padL,
-                  sizes.padL,
-                  bottomClearance,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TravelSearchBar(
-                      controller: _searchController,
-                      hintText: hintText,
-                      onChanged: (_) => setState(() {}),
-                      onSubmitted: _openSearchResults,
-                      textInputAction: TextInputAction.done,
-                      autofocus: false,
-                      focusNode: _focusNode,
-                    ),
-                    if (mode == SearchResearchMode.mates)
-                      Expanded(
-                        child: MatesVerticalSection(
-                          title: AppStrings.searchMatesTitle,
-                          mates: filteredMates,
-                          emptyMessage: AppStrings.searchNoMatesMessage,
-                          listHeight: double.infinity,
-                          onMateTap: _openMateDetails,
-                        ),
-                      )
-                    else
-                      Expanded(
-                        child: TripsVerticalSection(
-                          title: AppStrings.searchTripsTitle,
-                          trips: filteredTrips,
-                          emptyMessage: AppStrings.searchNoTripsMessage,
-                          listHeight: double.infinity,
-                          onTripTap: _openTripSchedule,
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-              Positioned(
-                right: sizes.padL,
-                bottom: sizes.padS,
-                child: SearchModeSwitchButton(
-                  mode: mode,
-                  onTap: SearchResearchModeStore.instance.toggle,
-                  tripsLabel: AppStrings.searchModeTripsLabel,
-                  matesLabel: AppStrings.searchModeMatesLabel,
-                  size: modeButtonSize,
-                ),
-              ),
-            ],
-          ),
+        return SearchModeView(
+          mode: mode,
+          controller: _searchController,
+          filteredMates: mode == SearchResearchMode.mates
+              ? filterMates(
+                  _mates,
+                  _searchController.text,
+                  limit: _maxMatesShown,
+                )
+              : const <MateProfile>[],
+          filteredTrips: mode == SearchResearchMode.trips
+              ? filterTrips(
+                  _tripTiles,
+                  _searchController.text,
+                  limit: _maxTripsShown,
+                )
+              : const <TripTileData>[],
+          onChanged: (_) => setState(() {}),
+          onSubmitted: _openSearchResults,
+          focusNode: _focusNode,
         );
       },
     );
